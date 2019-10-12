a1a69ef153bcd40373c9ca76cef2342e97f754d7
Dectinc Chen <chen@shijiang.info>
2019-10-12 13:10:05 +0800

    add db package

---
+++ src/evision/lib/db/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 eVision.ai Inc. All Rights Reserved.
+#
+# @author: Chen Shijiang(chenshijiang@evision.ai)
+# @date: 2019-10-12 11:20
+# @version: 1.0
+from ._error import ModelOfWrongType, ModelIsNone
+from ._field import CompatibleBlobField
+from ._dao import BaseModel, BaseDao
