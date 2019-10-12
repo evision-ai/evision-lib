1fb020079b320d9896de43e8b28146030575d7aa
Dectinc Chen <chen@shijiang.info>
2019-10-12 15:34:18 +0800

    rename utilities

--- src/evision/lib/util/__init__.py
+++ src/evision/lib/util/__init__.py
@@ -7,6 +7,6 @@
 # @date: 2019-10-11 17:32
 # @version: 1.0
 
-from ._cache_util import CacheUtil
-from ._path_util import PathUtil
+from ._cache import CacheUtil
+from ._path import PathUtil
 from ._type import TypeUtil
