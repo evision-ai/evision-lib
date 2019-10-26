f1149c5aa82b4cdf3e9b8f1e17063255b20ae723
Dectinc Chen <chen@shijiang.info>
2019-10-26 15:42:42 +0800

    remove tests in ImageSourcePreview

--- src/evision/lib/video/preview.py
+++ src/evision/lib/video/preview.py
@@ -13,7 +13,7 @@ import cv2
 
 from evision.lib.entity import ImageFrame, Vertex
 from evision.lib.util import DrawUtil
-from evision.lib.video import (BaseImageSource, ImageSourceType, ImageSourceWrapper, VideoCaptureSource)
+from evision.lib.video import BaseImageSource, ImageSourceWrapper
 
 
 class ImageSourcePreview(Thread):
@@ -28,6 +28,8 @@ class ImageSourcePreview(Thread):
             self.process = lambda x: x
 
     def _process_source_wrapper(self, image_frame: ImageFrame):
+        if not image_frame or image_frame.frame is None:
+            return None
         frame = image_frame.resized_frame
         DrawUtil.put_text(frame, 'Resized frame shape: {}'.format(image_frame.resized_size),
                           (10, 10))
@@ -38,46 +40,18 @@ class ImageSourcePreview(Thread):
         return frame
 
     def run(self):
-        while True:
+        while self.source.is_alive():
             frame = self.source.provide()
             if frame is None:
                 time.sleep(0.1)
                 continue
             frame = self.process(frame)
             if frame is None:
+                time.sleep(0.1)
                 continue
             cv2.imshow(self.source.name, frame)
 
             if cv2.waitKey(200) & 0xFF == ord('q'):
                 cv2.destroyWindow(self.source.name)
                 break
-        print('Finished preview')
-
-
-if __name__ == '__main__':
-    # video_file = 'D:/test.mp4'
-    # source = VideoCaptureImageSource(video_file, ImageSourceType.VIDEO_FILE,
-    #                                  fps=24, debug=True)
-    # source.daemon = True
-    # source.start()
-    # preview = ImageSourcePreview(source)
-    # preview.run()
-    # source.stop()
-
-    from evision.lib.video import ImageSourceWrapperConfig
-
-    source = 'rtsp://admin:1111aaaa@192.100.1.189:554/h264/ch1/main/av_stream'
-    source_type = ImageSourceType.IP_CAMERA
-
-    video_source = VideoCaptureSource(source=source, type=source_type,
-                                      width=960, height=480, fps=5)
-    wrapper_config = ImageSourceWrapperConfig(
-        width=960, height=540,
-        zone_start_x=30, zone_start_y=20, zone_width=900, zone_height=500)
-    wrapper = ImageSourceWrapper(video_source, wrapper_config)
-
-    video_source.daemon = True
-    video_source.start()
-    preview = ImageSourcePreview(wrapper)
-    preview.run()
-    video_source.stop()
+        cv2.destroyWindow(self.source.name)
