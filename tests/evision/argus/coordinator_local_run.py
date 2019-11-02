eb380367de5318ed827f9ffc1f63254f87cf6cc7
Dectinc Chen <chen@shijiang.info>
2019-11-02 13:52:32 +0800

    wip

--- tests/evision/argus/coordinator_local_run.py
+++ tests/evision/argus/coordinator_local_run.py
@@ -11,31 +11,42 @@
 import os
 import time
 
-from evision.argus.app import ArgusApplicationConfig, DummyApplication
+from evision.argus.app import App, ArgusApplicationConfig
 from evision.argus.coordinator import ArgusCoordinator, DictImageSourceCoordinator
 from evision.lib.video import ImageSourceConfig, ImageSourceType, ImageSourceWrapperConfig
-from evision.lib.video.source import VideoFileImageSource
-
-__local_video__ = os.path.expanduser('~/Downloads/test.avi')
+from evision.lib.video.source import VideoCaptureSource, VideoFileImageSource
 
 __wrapper_config__ = ImageSourceWrapperConfig(
     width=960, height=540,
     zone_start_x=80, zone_start_y=70, zone_width=800, zone_height=400)
 
+__local_video__ = os.path.expanduser('D:/test.mp4')
 image_source_config = ImageSourceConfig(
     source_uri=__local_video__, source_type=ImageSourceType.VIDEO_FILE,
-    extra={
-        'endless': True
-    })
+    handler_name='video_file', endless=True)
+
+__ip_camera__ = 'rtsp://admin:1111aaaa@192.100.1.189:554/h264/ch1/main/av_stream'
+ip_camera_config = ImageSourceConfig(
+    source_uri=__ip_camera__, source_type=ImageSourceType.IP_CAMERA,
+    handler_name='video_capture'
+)
 
 source_coordinator = DictImageSourceCoordinator()
 coordinator = ArgusCoordinator()
 
 
 def test_source_registering():
-    source = source_coordinator.register(image_source_config, VideoFileImageSource)
+    source = source_coordinator.register(image_source_config)
     assert isinstance(source, VideoFileImageSource)
     assert source.endless
+
+
+def test_ip_camera_registering():
+    source1 = source_coordinator.register(ip_camera_config)
+    assert isinstance(source1, VideoCaptureSource)
+    source2 = source_coordinator.register(ip_camera_config, VideoFileImageSource)
+    assert isinstance(source2, VideoCaptureSource)
+    assert source1 is source2
 
 
 def test_coordinator():
@@ -43,19 +54,23 @@ def test_coordinator():
         image_source_config=image_source_config,
         source_wrapper_config=__wrapper_config__,
         fps=0.5,
-        name='test-app1'
-    )
+        name='test-app1', app_handler=App.dummy)
     app_config2 = ArgusApplicationConfig(
         image_source_config=image_source_config,
         source_wrapper_config=__wrapper_config__,
         fps=0.2,
-        name='test-app2'
-    )
-    coordinator.add(app_config1, app_type=DummyApplication, source_type=VideoFileImageSource)
-    coordinator.add(app_config2, app_type=DummyApplication, source_type=VideoFileImageSource)
-    assert 1 == coordinator.n_image_sources
-    assert 2 == coordinator.n_apps
-    time.sleep(3)
+        name='test-app2', app_handler=App.dummy)
+    app_config3 = ArgusApplicationConfig(
+        image_source_config=ip_camera_config,
+        source_wrapper_config=__wrapper_config__,
+        fps=0.5,
+        name='test-app3', app_handler=App.dummy)
+    coordinator.add(app_config1)
+    coordinator.add(app_config2)
+    coordinator.add(app_config3)
+    assert 2 == coordinator.n_image_sources
+    assert 3 == coordinator.n_apps
+    time.sleep(10)
     coordinator.stop()
 
 
--- tests/evision/lib/video/video_preview.py
+++ tests/evision/lib/video/video_preview.py
@@ -7,11 +7,7 @@
 # @version: 1.0
 #
 
-from evision.lib.video import (
-    ImageSourceType, VideoCaptureSource,
-    ImageSourceWrapper, ImageSourceWrapperConfig,
-    ImageSourcePreview
-)
+from evision.lib.video import *
 from evision.lib.video.source import VideoFileImageSource
 
 width, height = 960, 540
@@ -31,16 +27,16 @@ def _open_image_source(source):
 
 
 def with_ip_camera():
-    source_uri = 'rtsp://admin:password@192.100.1.134:554/h264/ch1/main/av_stream'
+    source_uri = 'rtsp://admin:1111aaaa@192.100.1.189:554/h264/ch1/main/av_stream'
     source_type = ImageSourceType.IP_CAMERA
-    source = VideoCaptureSource(source=source_uri, source_type=source_type)
+    source = VideoCaptureSource(source_uri=source_uri, source_type=source_type)
     _open_image_source(source)
 
 
 def with_usb_camera():
     source_uri = 0
     source_type = ImageSourceType.USB_CAMERA
-    source = VideoCaptureSource(source=source_uri, source_type=source_type,
+    source = VideoCaptureSource(source_uri=source_uri, source_type=source_type,
                                 width=width, height=height)
     _open_image_source(source)
 
@@ -49,9 +45,11 @@ def with_video_file():
     import os
     source_uri = os.path.expanduser('~/Downloads/test.avi')
     source_type = ImageSourceType.VIDEO_FILE
-    source = VideoFileImageSource(source_uri, source_type)
+    source = VideoFileImageSource(source_uri=source_uri, source_type=source_type)
     _open_image_source(source)
 
 
 if __name__ == '__main__':
-    with_video_file()
+    with_ip_camera()
+    # with_video_file()
+    pass
