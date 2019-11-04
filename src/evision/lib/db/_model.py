db317935883594579efac0f4555c6a589ef78896
Chen Shijiang <chenshijiang@evision.ai>
2019-11-04 10:59:00 +0800

    add BaseVersionedModel

--- src/evision/lib/db/_model.py
+++ src/evision/lib/db/_model.py
@@ -17,6 +17,7 @@ from evision.lib.decorator import CachedProperty
 
 __all__ = [
     'BaseModel',
+    'BaseVersionedModel',
     'TimestampedModel'
 ]
 
@@ -41,6 +42,37 @@ class BaseModel(peewee.Model):
             return json.dumps(self.__dict__)
         except Exception:
             return str(self.__dict__)
+
+
+class BaseVersionedModel(BaseModel):
+    """包含乐观锁的数据库表结构封装"""
+    version = peewee.IntegerField(default=0)
+
+    def save_optimistic(self):
+        if not self.version:
+            return self.save()  # Since this is an `INSERT`, just call regular save method.
+
+        # Update any data that has changed and bump the version counter.
+        field_data = dict(self._data)
+        current_version = field_data.pop('version', 0)
+        field_data = self._prune_fields(field_data, self.dirty_fields)
+        if not field_data:
+            raise ValueError('No changes have been made.')
+
+        model_class = type(self)
+        field_data['version'] = model_class.version + 1  # Atomic increment
+
+        query = model_class.update(**field_data).where(
+            (model_class.version == current_version) &
+            (model_class.id == self.id))
+
+        nrows = query.execute()
+        if nrows == 0:
+            # It looks like another process has updated the version number.
+            raise peewee.PeeweeException('Conflict')  # Raise exception? Return False?
+        else:
+            self.version += 1  # Update in-memory version number.
+            return nrows
 
 
 class TimestampedModel(BaseModel):
