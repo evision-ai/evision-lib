9d315517f5eb7ad301f831cb6722279cfee138ec
Dectinc Chen <shijiang@aliyun.com>
2019-12-19 23:03:25 +0800

    refactor: remove argus related files

--- tests/evision/argus/__init__.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2019 eVision.ai Inc. All Rights Reserved.
-#
-# @author: Chen Shijiang(chenshijiang@evision.ai)
-# @date: 2019-12-03 09:15
-# @version: 1.0
-#
--- tests/evision/argus/video/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2019 eVision.ai Inc. All Rights Reserved.
-#
-# @author: Chen Shijiang(chenshijiang@evision.ai)
-# @date: 2019-10-17 14:44
-# @version: 1.0
